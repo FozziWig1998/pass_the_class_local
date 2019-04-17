5d03dfa03ea5d48ac5f85334ae5603856f68df71
parth patel <parthgp2@illinois.edu>
2019-04-16 19:27:18 -0500

    Fixed course not showing up on home page

--- passTheClass/public/index.php
+++ passTheClass/public/index.php
@@ -36,7 +36,7 @@
     <table>
         <thead>
             <tr>
-                <th>CRN</th>
+                <th>Course</th>
                 <th>Professor</th>
                 <th>Semester</th>
                 <th>Credit Hours</th>
@@ -45,7 +45,7 @@
         <tbody>
             <?php foreach ($result as $row) : ?>
                 <tr>
-                    <td><?php echo escape($row["CRN"]); ?></td>
+                    <td><?php echo escape($row["name"]); ?></td>
                     <td><?php echo escape($row["professor"]); ?></td>
                     <td><?php echo escape($row["semester"]); ?></td>
                     <td><?php echo escape($row["creditHours"]); ?></td>
@@ -102,6 +102,9 @@
       </li>
       <li>
         <a href="delete_student.php"><strong>Delete a Student</strong></a>
+      </li>
+      <li>
+        <a href="professors.php"><strong>Get all Professors</strong></a>
       </li>
     </ul>
 
